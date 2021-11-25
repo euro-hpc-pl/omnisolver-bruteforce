c9e8a79e0bb316e3a027f7fca2d0809d8748065f
Konrad Jałowiecki <dexter2206@gmail.com>
2021-11-25 10:40:36 +0100

    Add missing test for energy computation

--- tests/cpu/test_qubo.py
+++ tests/cpu/test_qubo.py
@@ -4,6 +4,18 @@ import pytest
 from omnisolver.bruteforce.cpu.qubo import qubo
 
 
+@pytest.fixture
+def q_mat():
+    return np.array(
+        [
+            [-16, 4, 12, -4],
+            [4, 15, -16, 4],
+            [12, -16, 0, 3],
+            [-4, 4, 3, -5],
+        ]
+    )
+
+
 class TestQubo:
     @pytest.mark.parametrize(
         "q_mat",
@@ -15,3 +27,14 @@ class TestQubo:
     def test_cannot_be_initialized_with_non_symmetric_coefficient_matrix(self, q_mat):
         with pytest.raises(ValueError):
             qubo(q_mat)
+
+    @pytest.mark.parametrize(
+        "state, expected_energy",
+        [
+            (np.zeros(4, dtype=np.int8), 0),  # sanity check
+            (np.ones(4, dtype=np.int8), -3),  # equal to sum of upper triangle
+            (np.array([0, 1, 0, 1], dtype=np.int8), 14),
+        ],
+    )
+    def test_correctly_computes_energy_given_state(self, q_mat, state, expected_energy):
+        assert qubo(q_mat).energy(state) == expected_energy
