c07a4592b9887d21d6c77178c9cb10b33cac75f3
Konrad Jałowiecki <dexter2206@gmail.com>
2023-03-28 22:18:29 +0200

    Add draft of distributed runner

---
+++ omnisolver/bruteforce/gpu/distributed.py
@@ -0,0 +1,87 @@
+from itertools import product
+import typing
+
+from dimod import append_variables, concatenate, Sampler, Vartype
+import numpy as np
+import ray
+
+from .sampler import BruteforceGPUSampler
+
+
+@ray.remote(num_gpus=1)
+def _solve_subproblem(
+    bqm,
+    num_states,
+    fixed_vars,
+    suffix_size,
+    grid_size,
+    block_size,
+    dtype
+):
+    new_bqm = bqm.copy()
+    new_bqm.fix_variables(fixed_vars)
+
+    sampler = BruteforceGPUSampler()
+    result = sampler.sample(
+        new_bqm,
+        num_states,
+        suffix_size,
+        grid_size,
+        block_size,
+        dtype
+    )
+
+    return append_variables(result, fixed_vars)
+
+
+class DistributedBruteforceGPUSampler(Sampler):
+
+    def sample(
+        self, bqm, num_states, num_fixed_vars, suffix_size, grid_size, block_size, dtype=np.float32
+    ):
+        if bqm.vartype == Vartype.SPIN:
+            return self.sample(
+                bqm.change_vartype("BINARY", inplace=False),
+                num_states,
+                suffix_size,
+                grid_size,
+                block_size,
+            ).change_vartype("SPIN", inplace=False)
+
+        bqm, mapping = bqm.relabel_variables_as_integers()
+
+        subproblems = [
+            {i: v for i, v in enumerate(vals)}
+            for vals in product([0, 1], repeat=num_fixed_vars)
+        ]
+
+        refs = [
+            _solve_subproblem.remote(
+                bqm,
+                num_states,
+                fixed_vars,
+                suffix_size,
+                grid_size,
+                block_size,
+                dtype
+            )
+            for fixed_vars in subproblems
+        ]
+
+        subsolutions = [ray.get(ref) for ref in refs]
+        return concatenate(subsolutions).truncate(num_states)
+
+
+    @property
+    def parameters(self) -> typing.Dict[str, typing.Any]:
+        return {
+            "num_states": [],
+            "suffix_size": [],
+            "grid_size": [],
+            "block_size": [],
+            "dtype": [],
+        }
+
+    @property
+    def properties(self) -> typing.Dict[str, typing.Any]:
+        return {}
